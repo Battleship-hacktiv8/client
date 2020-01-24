5574e8176ec7a804f12743821b0ab553e25ec184
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-01-24 15:31:07 +0700

    background video

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home d-flex justify-content-center align-items-center" id="landing">
+    <div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+      <iframe frameborder="0" height="100%" width="100%"
+        src="https://youtube.com/embed/lpebaayAJmQ?autoplay=1&controls=0&showinfo=0&autohide=1">
+      </iframe>
+    </div>
     <form @submit="toRoom" id="form-name" class="p-4">
       <div class="form-group">
         <label for="name">Insert your name:</label>
@@ -33,7 +38,8 @@ export default {
 
 <style scoped>
 #landing {
-  background-image: url('https://images.cgmagonline.com/wp-content/uploads/2018/02/world-of-warships-blitz-review-12-1280x720.jpg');
+  /* background-image: url('https://images.cgmagonline.com/wp-content/uploads/2018/02/world-of-warships-blitz-review-12-1280x720.jpg'); */
+  background-image: unset;
   background-size: cover;
   height: 100vh;
 }
